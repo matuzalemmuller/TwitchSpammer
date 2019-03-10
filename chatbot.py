f4e1c51941ced3bf9d5beb88b34e7febdeef9aea
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-03-10 12:26:00 -0300

    Improve socket handling
    * Add try/except to sockets and file opening
    * Switched from urllib2 to requests to check if channel is online
    * json lib now used to parse result of twitch online stream result
    * Add prints to describe app actions
    * Move messages file to resources folder

--- chatbot.py
+++ chatbot.py
@@ -1,35 +1,41 @@
 import socket
 import time
 import sys
-import urllib.request
 import random
+import requests
+import json
 
 # Twitch endpoint and interval between messages to be sent
 HOST = "irc.chat.twitch.tv"  # twitch irc server
-PORT = 6667  # port 
-MESSAGE_INTERVAL = 1200 # message interval in seconds
+PORT = 6667  # port
+MESSAGE_INTERVAL = 1200  # message interval in seconds
 
 
 # Checks if twitch channel is live
 def isChannelLive(clientId, channel):
-    url = str('https://api.twitch.tv/kraken/streams?client_id='+
+    url = str('https://api.twitch.tv/kraken/streams?client_id=' +
               clientId + "&channel=" + channel[1:])
     try:
-        result = urllib.request.urlopen(url)
-        result_decoded = result.read().decode("utf-8")
-        return int(result_decoded[10])
-    except urllib.error.URLError as e:
-        print(e.reason)
+        streamer_html = requests.get(url)
+        streamer = json.loads(streamer_html.content)
+        return streamer["_total"]
+    except requests.exceptions.RequestException as e:
+        print(e)
         return -1
 
 
 # Connects to Twitch IRC
 def connect(username, token, channel):
     s = socket.socket()
-    s.connect((HOST, PORT))
-    s.send("PASS {}\r\n".format(token).encode("utf-8"))
-    s.send("NICK {}\r\n".format(username).encode("utf-8"))
-    s.send("JOIN {}\r\n".format(channel).encode("utf-8"))
+    try:
+        s.connect((HOST, PORT))
+        s.send("PASS {}\r\n".format(token).encode("utf-8"))
+        s.send("NICK {}\r\n".format(username).encode("utf-8"))
+        s.send("JOIN {}\r\n".format(channel).encode("utf-8"))
+        print("Connected to twitch channel " + channel + " as " + username)
+    except socket.error as e:
+        print(e)
+        sys.exit(1)
     return s
 
 
@@ -38,20 +44,25 @@ def main():
         print("Usage: twitchbot <username> <token> <channel>")
         sys.exit(1)
 
-    username  = sys.argv[1]
+    username = sys.argv[1]
     clientId = sys.argv[2]
     if sys.argv[3][:6] != "oauth:":
         token = "oauth:" + sys.argv[3]
     else:
         token = sys.argv[3]
-    channel   = "#" + sys.argv[4]
+    channel = "#" + sys.argv[4]
 
-    socket = connect(username, token, channel)
+    s = connect(username, token, channel)
 
     # Loads Tanner pastas from tanner.txt file
-    text_file = open("tanner.txt",'r')
-    data = text_file.read()
-    messages = data.split("\n")
+    try:
+        text_file = open("resources/tanner.txt", 'r')
+        data = text_file.read()
+        messages = data.split("\n")
+        print("Messages loaded from tanner.txt file")
+    except IOError:
+        print("File tanner.txt is not available")
+        sys.exit(1)
 
     # Replaces "Octavian", "Kripp" and "Kripparian" in Tanner pastas by
     # channel name if chanel is not nl_Kripp
@@ -64,10 +75,21 @@ def main():
     # is online
     while True:
         if isChannelLive(clientId, channel) > 0:
-            text = "PRIVMSG {} :{}".format(channel, random.choice(messages))
+            print("Channel " + channel + " is online")
+            message = random.choice(messages)
+            text = "PRIVMSG {} :{}".format(channel, message)
             text = text + "\r\n"
-            socket.send(text.encode('utf-8'))
+            try:
+                s.send(text.encode('utf-8'))
+                print("Sent message: " + message)
+            except socket.error as e:
+                print(e)
+        else:
+            print("Channel " + channel + " is offline")
+
+        print("Waiting " + str(MESSAGE_INTERVAL) + " seconds...")
         time.sleep(MESSAGE_INTERVAL)
+        print("Time to send message!")
 
 
 if __name__ == "__main__":
