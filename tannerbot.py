741b1a591e76871eac04b4350e6baddcb62a8629
Matuzalem Muller <matuzalemmuller@gmail.com>
2019-03-12 21:05:58 -0300

    Add message for test completion

--- tannerbot.py
+++ tannerbot.py
@@ -126,6 +126,7 @@ def main():
 
         # Test condition for CI/CD integration
         if os.environ.get("TEST") == "1":
+            print("This was a successfull test")
             sys.exit(0)
 
         # Wait to send another message
